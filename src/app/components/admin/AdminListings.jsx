ecc462a3d2f8e0305e21fd5c63a57efdeed53422
Pablo Valdes <pablovaldes0925@gmail.com>
2025-04-30 01:23:04 -0400

    magnifying glass tweak

--- src/app/components/admin/AdminListings.jsx
+++ src/app/components/admin/AdminListings.jsx
@@ -258,7 +258,7 @@ export default function AdminListings() {
               onChange={(e) => setSearchQuery(e.target.value)}
               className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#FF0056]"
             />
-            <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+            <div className="absolute left-3 top-[22%] transform -translate-y-1/2 text-gray-400">
               <Search size={24} strokeWidth={2.5} color="#FF0056" />
             </div>
           </div>
